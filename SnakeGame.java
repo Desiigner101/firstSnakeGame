90e3c4f1f3f7b1c4d2af49530b3590275176f57a
Gino Sarsonas <desiigner4074@gmail.com>
2024-08-26 22:43:31 -0700

    first commit

--- SnakeGame.java
+++ SnakeGame.java
@@ -16,21 +16,3 @@ public class SnakeGame extends JPanel{
         setBackground(Color.black);
     }
 }
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Random;
-import javax.swing.*;
-
-
-public class SnakeGame extends JPanel{
-    int boardWidth;
-    int boardHeight;
-
-    SnakeGame(int boardWidth, int boardHeight){
-        this.boardWidth = boardWidth;
-        this.boardHeight = boardHeight;
-        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
-        setBackground(Color.black);
-    }
-}
